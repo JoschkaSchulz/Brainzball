59e9f26b5b9ab556857869a8d1f03f43330faecb
J. Schulz <Horstyschulz@gmail.com>
2014-02-22 15:57:29 +0100

    correct name of ResourceLoader added reference to game and menu

--- brainzballs/src/de/brainzballs/game/Game.java
+++ brainzballs/src/de/brainzballs/game/Game.java
@@ -18,6 +18,7 @@ public class Game extends Group {
 	public Game(BrainzBalls brainzBalls) {
 		this.brainzBalls = brainzBalls;
 		
-		
+		this.field = Field.newInstance(21, 11);
+		addActor(this.field);
 	}
 }
