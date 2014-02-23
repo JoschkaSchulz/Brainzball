2478d78637066a65c8768e14c343709692415e6c
J. Schulz <Horstyschulz@gmail.com>
2014-02-24 00:56:37 +0100

    added music :D

--- brainzballs/src/de/brainzballs/BrainzBalls.java
+++ brainzballs/src/de/brainzballs/BrainzBalls.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
@@ -16,6 +17,7 @@ public class BrainzBalls implements ApplicationListener {
 	
 	private MainMenu mainMenu;
 	private Game game;
+	private Music music;
 	
 	public void create () {
 	    stage = new Stage();
@@ -32,6 +34,10 @@ public class BrainzBalls implements ApplicationListener {
 	    
 	    //Adding the menu to stage
 	    stage.addActor(mainMenu);
+	    
+	    music = Gdx.audio.newMusic(Gdx.files.internal("data/music/music1.mp3"));
+	    music.setLooping(true);
+	    music.play();
 	}
 
 	public void resize (int width, int height) {
